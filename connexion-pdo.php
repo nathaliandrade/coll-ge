c1d39c668c6858d3f0d940914043b7e538775fb2
Nathalia Andrade <116752892+nathaliandrade@users.noreply.github.com>
2022-11-09 15:07:22 -0500

    Update connexion-pdo.php

--- connexion-pdo.php
+++ connexion-pdo.php
@@ -1,10 +1,9 @@
 <?php
 
-$servname = "localhost"; $dbname = "Projet"; $port = 3306; $user = "root"; $pass = "";
+$servname = "localhost"; $dbname = "hzol3721_osullivan"; $user = "hzol3721_osullivan"; $pass = "Np030218!";
 try{
     $conn = new PDO("mysql:host=$servname;dbname=$dbname;port=$port", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("set names utf8mb4");
     }
     catch(PDOException $e){
     echo "Erreur : " . $e->getMessage();
